aea1477ee3070135579a691504ec28945d00b856
amarjeetmaan3 <amarjeetmaan33@gmail.com>
2026-09-19 14:27:59 +0530

    Create ControllerWebView.kt

---
+++ app/src/main/java/com/smartoverlay/controller/ControllerWebView.kt
@@ -0,0 +1,59 @@
+package com.smartoverlay.controller
+
+import android.content.Context
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class ControllerWebView(
+    context: Context
+) : WebView(context) {
+
+    init {
+
+        settings.apply {
+
+            javaScriptEnabled = true
+
+            domStorageEnabled = true
+
+            databaseEnabled = true
+
+            allowFileAccess = true
+
+            allowContentAccess = true
+
+            cacheMode = WebSettings.LOAD_DEFAULT
+
+            mediaPlaybackRequiresUserGesture = false
+
+            setSupportZoom(false)
+
+            builtInZoomControls = false
+
+            displayZoomControls = false
+        }
+
+        webViewClient = WebViewClient()
+
+        addJavascriptInterface(
+            WebAppBridge(context),
+            "SmartOverlayAndroid"
+        )
+    }
+
+    fun loadController() {
+
+        /*
+         * Phase 12C APK will package the final
+         * SmartOverlay controller web files here.
+         *
+         * Expected asset:
+         * app/src/main/assets/controller.html
+         */
+
+        loadUrl(
+            "file:///android_asset/controller.html"
+        )
+    }
+}
