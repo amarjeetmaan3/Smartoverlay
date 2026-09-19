538f4967a6aff1d7b37f4bbde46f46f6856874dc
amarjeetmaan3 <amarjeetmaan33@gmail.com>
2026-09-19 14:27:14 +0530

    Update MainActivity.kt

--- app/src/main/java/com/smartoverlay/controller/MainActivity.kt
+++ app/src/main/java/com/smartoverlay/controller/MainActivity.kt
@@ -1,1 +1,31 @@
-z
+package com.smartoverlay.controller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var controllerWebView: ControllerWebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        controllerWebView = ControllerWebView(this)
+
+        setContentView(controllerWebView)
+
+        controllerWebView.loadController()
+    }
+
+    override fun onBackPressed() {
+
+        if (controllerWebView.canGoBack()) {
+
+            controllerWebView.goBack()
+
+        } else {
+
+            super.onBackPressed()
+        }
+    }
+}
