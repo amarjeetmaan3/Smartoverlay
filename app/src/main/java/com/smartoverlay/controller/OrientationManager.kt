b42295125c3e40dd858ee5db48d22ed7888a3c4d
amarjeetmaan3 <amarjeetmaan33@gmail.com>
2026-09-19 14:35:36 +0530

    Create OrientationManager.kt

---
+++ app/src/main/java/com/smartoverlay/controller/OrientationManager.kt
@@ -0,0 +1,29 @@
+package com.smartoverlay.controller
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+
+class OrientationManager(
+    private val activity: Activity
+) {
+
+    fun portrait() {
+        activity.requestedOrientation =
+            ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+
+    fun landscape() {
+        activity.requestedOrientation =
+            ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    }
+
+    fun sensor() {
+        activity.requestedOrientation =
+            ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+    }
+
+    fun unspecified() {
+        activity.requestedOrientation =
+            ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+    }
+}
